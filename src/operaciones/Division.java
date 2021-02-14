6704256464447cbb48862082e57f43d69c9dae8b
PedroMurilloArnau <76122975+PedroMurilloArnau@users.noreply.github.com>
2021-02-14 22:57:27 +0100

    Creacion de la clase division

---
+++ src/operaciones/Division.java
@@ -0,0 +1,53 @@
+package operaciones;
+/**
+*En este metodo se llevaran a cavo los metodos relacionado con el conciente entre dos numero.
+*
+*@version 1.0
+*@autho Pedro Murillo
+*@since 14/02/2020
+*
+*/
+	public class Division {
+		/**
+		 *En este metodo se llevaran a cavo los metodos relacionado con la division de dos numero
+		 * reales
+		 * @param num1 numero real 1.
+		 * @param num2 numero real 2.
+		 * @return un double, division (num1, num2).
+		 */
+		public static double dividision2reales(double num1, double num2) {
+			return num1 / num2;
+		}
+
+		/**
+		 * Es el metodo en el que se lleva a cabo la division entre dos numeros enteros
+		 * 
+		 * @param num1 entero numero 1.
+		 * @param num2 entero numero 2.
+		 * @return un integer, deivision de (num1 entre num2).
+		 */
+		public static int division2enteros(int num1, int num2) {
+			return num1 / num2;
+		}
+
+		/**
+		 * Es el método para calcular el inverso de un número
+		 * 
+		 * @param num1 numero real 1.
+		 * @return un double, inverso de num1
+		 */
+		public static double inversoreal(double num1) {
+			return 1/num1;
+		}
+
+		/**
+		 * Raiz de un número entero
+		 * 
+		 * @param num1 numero real 
+		 * @return un double, la raiz de num1
+		 */
+		public static double raiz(double num1) {
+			return Math.sqrt(num1);
+		}
+
+	}
