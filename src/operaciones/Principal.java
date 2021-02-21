0434348e802b5d01fd9eedbb91b9e4768b479336
Irene-Toca <61660913+Irene-Toca@users.noreply.github.com>
2021-02-21 20:57:55 +0100

    Modificación del método valorAcumulado(), y de su ejecución desde la clase principal.

--- src/operaciones/Principal.java
+++ src/operaciones/Principal.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 /**
  * Esta clase es para ejecutar los métodos de las diferentes clases
  *
- * @author rober, Irene, F�lix
+ * @author rober, Irene, F�lix, Pedro
  ** @version 1.0
  * @since 14-02-2021 .
  */
@@ -28,19 +28,19 @@ public class Principal {
 		Suma suma1 = new Suma();
 		double num1Real;
 		double num2Real;
-
+	
 		Scanner entrada = new Scanner(System.in);
 		entrada.useLocale(Locale.US);
-
+	
 		System.out.println(
-				"¿Que tipo de operacion quieres realizar? \nIntroduce suma, resta, multiplicacion o division");
-
+				"¿Que operación quieres realizar? \nIntroduce suma, resta, multiplicacion o division");
+	
 		// Entrada por teclado. Obligamos a que la palabra se vaya a minisculas
 		String operacion = entrada.nextLine().toLowerCase();
-
+	
 		// Menu operación para seleccionar segun lo teclado
 		switch (operacion) {
-
+	
 		case "suma":
 			// Otro menú para seleccionar las operaciones de la Suma
 			System.out.println("Operación Suma");
@@ -48,43 +48,43 @@ public class Principal {
 			System.out.println("Pulse 2 para sumar dos números enteros ");
 			System.out.println("Pulse 3 para sumar tres números reales");
 			System.out.println("Pulse 4 para sumar el valor acumulado de tres números");
-
+	
 			// Entrada por teclado
 			int operacionSuma = entrada.nextInt();
-
+	
 			// Switch de selección de operaciones en el menú
 			switch (operacionSuma) {
-
+	
 			case 1:
 				System.out.println("\nPRIMER MÉTODO  SUMA DE  2 REALES\n");
 				System.out.println("Introduce un número");
-
+	
 				a = entrada.nextDouble();
 				System.out.println(a);
 				System.out.println("Introduce otro número");
 				b = entrada.nextDouble();
 				System.out.println(b);
-
+	
 				resultado = suma1.suma2Reales(a, b);
 				System.out.println("El resultado de la suma es: " + resultado);
 				System.out.println("\n***********************\n");
 				break;
-
+	
 			case 2:
 				System.out.println("\nSEGUNDO MÉTODO  SUMA DE 2 ENTEROS\n");
-
+	
 				System.out.println("Introduce un número");
 				a1 = entrada.nextInt();
 				System.out.println(a1);
 				System.out.println("Introduce otro número");
 				b1 = entrada.nextInt();
 				System.out.println(b1);
-
+	
 				solucion = suma1.suma2Enteros(a1, b1);
 				System.out.println("El resultado de la suma es: " + solucion);
 				System.out.println("\n***********************\n");
 				break;
-
+	
 			case 3:
 				System.out.println("\nTERCER MÉTODO  SUMA DE 3 REALES\n");
 				System.out.println("Introduce un número");
@@ -95,37 +95,27 @@ public class Principal {
 				System.out.println(b);
 				System.out.println("Introduce último número");
 				c = entrada.nextDouble();
-
+	
 				resultado = suma1.suma3Reales(a, b, c);
 				System.out.println("El resultado de la suma es: " + resultado);
 				System.out.println("\n***********************\n");
 				break;
-
+	
 			case 4:
-				System.out.println("\nCUARTO MÉTODO ACUMULANDO \nSe repetirá tres veces");
-				System.out.println("Introduce un número");
-				a1 = entrada.nextInt();
-				solucion = suma1.valorAcumulado(a1);
-				System.out.println("El valor acumulado es : " + solucion);
-
-				System.out.println("Introduce otro número");
-				a1 = entrada.nextInt();
-				solucion = suma1.valorAcumulado(a1);
-				System.out.println("El valor acumulado es : " + solucion);
-
-				System.out.println("Introduce el último número");
-				a1 = entrada.nextInt();
-				solucion = suma1.valorAcumulado(a1);
+				System.out.println("\nCUARTO MÉTODO ACUMULANDO");
+				
+				solucion = suma1.valorAcumulado();
+				
 				System.out.println("FIN. El valor acumulado es : " + solucion);
 				break;
-
+	
 			default:
 				System.out.println("La opción seleccionada no es correcta \nFin del programa");
-
+	
 			}
-
+	
 			break;
-
+	
 		// Opciones de resta
 		case "resta":
 			System.out.println("Operación Resta");
@@ -133,15 +123,15 @@ public class Principal {
 			System.out.println("Pulse 2 para restar dos números enteros");
 			System.out.println("Pulse 3 para restar tres números reales");
 			System.out.println("Pulse 4 para ir restando números hasta llegar a 0 o inferior");
-
+	
 			// Entrada por teclado
 			int operacionResta = entrada.nextInt();
-
+	
 			// Otro menu para seleccionar las operaciones de la resta
 			switch (operacionResta) {
-
+	
 			// Pasamos dos variables y llamamos al método resta2Reales de clase Resta
-
+	
 			case 1:
 				System.out.println("Introduzca primer digito");
 				num1Real = entrada.nextDouble();
@@ -150,7 +140,7 @@ public class Principal {
 				System.out.println("El resultado es: " + Resta.resta2Reales(num1Real, num2Real));
 				break;
 			// Pasamos dos variables y llamamos al metodo resta2Enteros de clase Resta
-
+	
 			case 2:
 				System.out.println("Introduzca primer digito");
 				int num1Entero = entrada.nextInt();
@@ -158,7 +148,7 @@ public class Principal {
 				int num2Entero = entrada.nextInt();
 				System.out.println("El resultado es: " + Resta.resta2Enteros(num1Entero, num2Entero));
 				break;
-
+	
 			// Pasamos tres variables y llamamos al metodo resta3Reales de clase Resta
 			case 3:
 				System.out.println("Introduzca primer digito");
@@ -169,17 +159,17 @@ public class Principal {
 				double num5Real = entrada.nextDouble();
 				System.out.println("El resultado es: " + Resta.resta3Reales(num3Real, num4Real, num5Real));
 				break;
-
+	
 			// Llamamos directamente a restaAcumulado de clase Resta donde realizaremos las
 			// operaciones
 			case 4:
-
+	
 				Resta.restaAcumulado();
-
+	
 				break;
-
+	
 			}
-
+	
 			break;
 		case "multiplicacion":
 			System.out.println("Operacion Producto");
@@ -187,16 +177,16 @@ public class Principal {
 			System.out.println("Pulse 2 para multiplicar dos números enteros");
 			System.out.println("Pulse 3 para multiplicar tres números reales");
 			System.out.println("Pulse 4 para exponenciar el primer n�mero introducido por el segundo");
-
+	
 			// Entrada por teclado
 			int operacionProducto = entrada.nextInt();
-
+	
 			// Switch de selecci�n de operaci�n en men�
 			switch (operacionProducto) {
-
+	
 			// Pasamos dos variables y llamamos al metodo multiplica2Reales de clase
 			// Producto
-
+	
 			case 1:
 				System.out.println("Introduzca primer digito");
 				num1Real = entrada.nextDouble();
@@ -204,7 +194,7 @@ public class Principal {
 				num2Real = entrada.nextDouble();
 				System.out.println("El resultado es: " + Producto.multiplica2reales(num1Real, num2Real));
 				break;
-
+	
 			// Pasamos dos variables y llamamos al metodo multiplica2enteros de clase
 			// Producto
 			case 2:
@@ -214,7 +204,7 @@ public class Principal {
 				int num2Entero = entrada.nextInt();
 				System.out.println("El resultado es: " + Producto.multiplica2enteros(num1Entero, num2Entero));
 				break;
-
+	
 			// Pasamos tres variables y llamamos al metodo multiplica3reales de clase
 			// Producto
 			case 3:
@@ -226,7 +216,7 @@ public class Principal {
 				double num3Real = entrada.nextDouble();
 				System.out.println("El resultado es: " + Producto.multiplica3reales(num1Real, num2Real, num3Real));
 				break;
-
+	
 			// Pasamos dos variables y llamamos al metodo potencia
 			case 4:
 				System.out.println("Introduzca primer digito");
@@ -235,18 +225,64 @@ public class Principal {
 				num2Real = entrada.nextDouble();
 				System.out.println("El resultado es: " + Producto.potencia(num1Real, num2Real));
 				break;
-
+	
 			}
-
+	
 			break;
-		case "división":
-			System.out.println("es la división");
-			break;
+		// Opciones de division
+			
+		case "division" :
+			System.out.println("Operacion Division");
+			System.out.println("Pulse 1 para division de dos números reales");
+			System.out.println("Pulse 2 para division de dos números enteros");
+			System.out.println("Pulse 3 para calcular el inverso de un número real");
+			System.out.println("Pulse 4 para calcular la raiz cuadra da un número real");
 
+
+			// Entrada por teclado
+			int operacionDivision = entrada.nextInt();
+			
+			// Switch de la seleccion de una operacion de division
+			switch (operacionDivision) {
+			
+			case 1:
+				System.out.println("Introduzca el devidiento primer termino");
+				num1Real = entrada.nextDouble();
+				System.out.println("Introduzca el divisor segundo termino");
+				num2Real = entrada.nextDouble();
+				System.out.println("El resultado es: " + Division.dividision2reales(num1Real, num2Real));
+				break;
+	
+			// Pasamos dos variables y llamamos al metodo division2reales
+			// Producto
+			case 2:
+				System.out.println("Introduzca el devidiento primer termino");
+				int num1Entero = entrada.nextInt();
+				System.out.println("Introduzca el divisor segundo termino");
+				int num2Entero = entrada.nextInt();
+				System.out.println("El resultado es: " + Division.division2enteros(num1Entero, num2Entero));
+				break;
+	
+				
+			// Pasamos una variable y llamamos al metodo inversoreal
+			case 3:
+				System.out.println("Introduzca primer digito");
+				num1Real = entrada.nextDouble();
+				System.out.println("El resultado es: " + Division.inversoreal(num1Real));
+				break;
+
+				// Pasamos una variable y llamamos al metodo raiz
+			
+			case 4:
+				System.out.println("Introduzca el dígito a calcular");
+				num1Real = entrada.nextDouble();
+				System.out.println("El resultado es: " + Division.raizreal(num1Real));
+				break;
+			}
+	
 		default:
 			System.out.println("La operación introducida no es valida \nFin del programa.");
 		}
-
+	
 	}
-
 }
