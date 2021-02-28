6e68fd90ab36dec2df7986862838e41596694275
FCdelasCuevas <felixcartondlc@gmail.com>
2021-02-28 11:48:01 +0000

    Hotfix DivisionTest

--- src/tests/DivisionTest.java
+++ src/tests/DivisionTest.java
@@ -1,7 +1,7 @@
 package tests;
 
 /**
- * @autor Fï¿½lix Cartï¿½n
+ * @autor Félix Cartón
  */
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
@@ -12,15 +12,9 @@ import operaciones.Division;
 public class DivisionTest {
 
 	/**
-	 * Comprobamos que el mï¿½todo diviDIsion2reales, en efecto, diDIvide dos reales.
+	 * Comprobamos que el método diviDIsion2reales, en efecto, diDIvide dos reales.
 	 */
-	@Test
-	public void testdividision2realescero() {
-		double resultado = Division.dividision2reales(1.2, 0);
-		double esperado = 0;
-
-		assertEquals(esperado, resultado);
-	}
+	
 	@Test
 	public void testdividision2reales() {
 		double resultado = Division.dividision2reales(1.2,2);
@@ -28,6 +22,19 @@ public class DivisionTest {
 
 				assertEquals(esperado, resultado);
 	}
+	/**
+	 * Comprobamos que al dividir entre cero, se retorna cero.
+	 */
+	@Test
+	public void testdividision2realescero() {
+		double resultado = Division.dividision2reales(1.2, 0);
+		double esperado = 0;
+
+		assertEquals(esperado, resultado);
+	}	
+	/**
+	 * Comprobamos que divide números negativos.
+	 */
 	@Test
 	public void testdividision2realesnegativos() {
 		double resultado = Division.dividision2reales(-1.4,2);
@@ -35,8 +42,9 @@ public class DivisionTest {
 
 				assertEquals(esperado, resultado);
 	}
+	
 	/**
-	 * Comprobamos que el mï¿½todo division2enteros, en efecto, divide dos enteros.
+	 * Comprobamos que el método division2enteros, en efecto, divide dos enteros.
 	 */
 	@Test
 	public void testdivision2enteros() {
@@ -45,6 +53,9 @@ public class DivisionTest {
 
 		assertEquals(esperado, resultado);
 	}
+	/**
+	 * Comprobamos que al dividir entre cero, se retorna cero.
+	 */
 	@Test
 	public void testdivision2enteroscero() {
 		double resultado = Division.division2enteros(6, 0);
@@ -52,6 +63,9 @@ public class DivisionTest {
 
 		assertEquals(esperado, resultado);
 	}
+	/**
+	 * Comprobamos que divide negativos.
+	 */
 	@Test
 	public void testdivision2enterosnegativos() {
 		double resultado = Division.division2enteros(6, -3);
@@ -59,8 +73,9 @@ public class DivisionTest {
 
 		assertEquals(esperado, resultado);
 	}
+	
 	/**
-	 * Comprobamos que el mï¿½todo inversoreal, en efecto, realiza la operaciï¿½n.
+	 * Comprobamos que el método inversoreal, en efecto, realiza la operación.
 	 */
 	@Test
 	public void testinversoreal() {
@@ -69,6 +84,9 @@ public class DivisionTest {
 
 		assertEquals(esperado, resultado);
 	}
+	/**
+	 * Comprobamos que al hacer el inverso de un número negativo, se retorna un número negativo.
+	 */
 	@Test
 	public void testinversorealnegativo() {
 		double resultado = Division.inversoreal(-10);
@@ -76,6 +94,9 @@ public class DivisionTest {
 
 		assertEquals(esperado, resultado);
 	}
+	/*
+	 * Comprobamos que al hacer el inverso de cero, se retorna cero.
+	 */
 	@Test
 	public void testinversorealcero() {
 		double resultado = Division.inversoreal(0);
@@ -83,6 +104,9 @@ public class DivisionTest {
 
 		assertEquals(esperado, resultado);
 	}
+	/**
+	 * Comprobamos que realiza la operación raíz cuadrada.
+	 */
 	@Test
 	public void testraizreal() {
 		double resultado = Division.raizreal(4);
@@ -90,5 +114,30 @@ public class DivisionTest {
 
 		assertEquals(esperado, resultado);
 	}
+	//Código obsoleto. Se modificó división para controlar la excepción de dividir por cero retornando cero.
+//	/**
+//     * El test comprueba que los métodos lancen una excepción al dividir entre cero.
+//     */
+//    @Test
+//    public void testdivisionentrecero() {
+//        Exception exception1 = assertThrows(ArithmeticException.class, () -> {
+//            Division.division2reales(1, 0);
+//        });
+//        String expectedMessage = "/ by zero";
+//        String actualMessage1 = exception.getMessage();
+//
+//        Exception exception2 = assertThrows(ArithmeticException.class, () -> {
+//            Division.division2enteros(3, 0);
+//        });
+//        String actualMessage2 = exception.getMessage();
+//
+//        Exception exception3 = assertThrows(ArithmeticException.class, () -> {
+//            Division.inversoreal(0);
+//        });
+//        String actualMessage3 = exception.getMessage();
+//        assertTrue(actualMessage1.contains(expectedMessage));
+//        assertTrue(actualMessage2.contains(expectedMessage));
+//        assertTrue(actualMessage3.contains(expectedMessage));
+//    }
 
 }
